f463e759f7721373d3221710648aec3212c9603b
Ivan Kopcik <ivan.kopcik@gmail.com>
2020-08-05 11:50:40 +0200

    :sparkles: GENERAL - added support for expanding fields in products

--- api/v1/products/index.js
+++ api/v1/products/index.js
@@ -1,31 +1,44 @@
-import _, { get } from "lodash";
+import { get } from "lodash";
+import withSession from "grandus-lib/utils/session";
 import {
   reqGetHeaders,
   reqApiHost,
   processQueryFilter,
   forwardPaginationHeaders,
-  getProductCardFields
+  getProductCardFields,
 } from "grandus-lib/utils";
 
-export default async (req, res) => {
+export default withSession(async (req, res) => {
+  const requestBody = {
+    orderBy: "priority-desc",
+  };
+
+  if (get(req, "query")) {
+    requestBody.param = processQueryFilter(get(req, "query"));
+
+    if (get(req, "query.category")) {
+      requestBody.categoryName = get(req, "query.category", "");
+    }
+
+    if (get(req, "query.productIds")) {
+      requestBody.productIds = get(req, "query.productIds", []);
+    }
+  }
+
+  const fields =
+    getProductCardFields() +
+    (get(req, "query.fields") ? `,${get(req, "query.fields")}` : "");
+
   const products = await fetch(
-    `${reqApiHost(req)}/api/v2/products/filter?fields=${getProductCardFields()}&page=${get(
+    `${reqApiHost(req)}/api/v2/products/filter?fields=${fields}&page=${get(
       req,
       "query.page",
       1
-    )}&per-page=${get(req, "query.perPage", 3)}&orderBy=${get(
-      req,
-      "query.orderBy",
-      "time-desc"
-    )}`,
+    )}&per-page=${get(req, "query.perPage", 3)}`,
     {
       method: "post",
       headers: reqGetHeaders(req),
-      body: JSON.stringify({
-        categoryName: get(req, "query.category", ""),
-        param: processQueryFilter(get(req, "query")),
-        orderBy: "time-desc",
-      }),
+      body: JSON.stringify(requestBody),
     }
   )
     .then((result) => {
@@ -37,7 +50,7 @@ export default async (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(products));
-};
+});
 
 export const config = {
   api: {
