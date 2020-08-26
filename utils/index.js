1bedf726df4977bfa920851f239167588f9ed7c0
matejkopcik <matej.kopcik@gmail.com>
2020-08-26 18:25:57 +0200

    :bulb: UTILS - added documentation to new function getImageUrl

--- utils/index.js
+++ utils/index.js
@@ -49,6 +49,19 @@ export const scrollToTop = () => {
   if (typeof window !== "undefined") window.scrollTo(0, 0);
 };
 
+/**
+ * Generates image path for certain image.
+ * Automaticaly add host and construct path by provided attributes.
+ * Url is constructed as :
+ * - host from environment (.env.*)
+ * - path from image object (image.path)
+ * - size provided size of image (resolutionXxResolutionY)
+ * - tyoe format of image to be generated (.jpg, .png, .webp)
+ * 
+ * @param {object} image
+ * @param {string} size 
+ * @param {string} type 
+ */
 export const getImageUrl = (image, size, type) => {
   const host = process.env.NEXT_PUBLIC_IMAGE_HOST
     ? process.env.NEXT_PUBLIC_IMAGE_HOST
