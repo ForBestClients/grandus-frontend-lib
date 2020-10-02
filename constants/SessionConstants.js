382e54bfebb1ceba724f947a0cdf7d087aa1b456
Ivan Kopcik <ivan.kopcik@gmail.com>
2020-10-02 16:53:07 +0200

    :sparkles: GENERAL - added cookie consent constant

--- constants/SessionConstants.js
+++ constants/SessionConstants.js
@@ -2,8 +2,13 @@
 export const GENERAL_CONSTANT = "_grandus-frontend-secure-session";
 
 //SESSIONS
+// - USER
 export const USER_CONSTANT = "_grandus_user";
 export const USER_COMPARE_CONSTANT = "_grandus_user_compare";
 export const USER_WISHLIST_CONSTANT = "_grandus_user_wishlist";
+// - CART
 export const CART_CONSTANT = "_grandus_cart";
 export const CART_CONTACT_CONSTANT = "_grandus_cart_contact";
+
+// - LEGAL
+export const LEGAL_COOKIE_CONSENT = "_grandus_cookie_consent";
