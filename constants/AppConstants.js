5550bb84be91db7c6a989d95bef1462def958cd0
Pavol Rasev <pavol@forbestclients.com>
2021-02-18 14:59:28 +0100

    :sparkles: GENERA - added new additional info type constant

--- constants/AppConstants.js
+++ constants/AppConstants.js
@@ -7,6 +7,7 @@ export const ATTACHMENT_TYPE_FILE = 1;
 export const ATTACHMENT_TYPE_URL = 2;
 export const ATTACHMENT_EXTENSION_PDF = 'pdf';
 
+export const ADDITIONAL_INFOS_TYPE_SIZES_TABLE = 1;
 export const ADDITIONAL_INFOS_TYPE_PERSON = 2;
 export const ADDITIONAL_INFOS_STORE_MANAGER_HASH = 'store_manager';
 
