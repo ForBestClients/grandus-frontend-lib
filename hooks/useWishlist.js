d84b014effa98a5764ce2e8051598f7053edf7d8
Pavol Rasev <pavol@forbestclients.com>
2021-04-01 13:52:19 +0200

    :sparkles: GENERAL - guest wishlist

--- hooks/useWishlist.js
+++ hooks/useWishlist.js
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import useSWR from "swr";
-import { isFunction, find, get, parseInt } from "lodash";
-import { transformWishlist } from "grandus-lib/utils/transformers";
+import { get, isFunction } from "lodash";
 
 const useWishlist = () => {
   const [isLoading, setIsLoading] = useState(false);
-
-  const { data, mutate, isValidating } = useSWR(
+  const { data: wishlist, mutate, isValidating } = useSWR(
     `/api/lib/v1/wishlist`,
     (url) => fetch(url).then((r) => r.json()),
     {
@@ -16,43 +14,18 @@ const useWishlist = () => {
     }
   );
 
-  let wishlist = transformWishlist(data);
-
-  const getItem = (productId) => {
-    return find(
-      wishlist?.items,
-      (item) => get(item, "product.id") == productId
-    );
-  };
-
   const itemExists = (productId) => {
-    return getItem(productId) ? true : false;
+    return (
+      get(wishlist, "productIds", []).findIndex(
+        (element) => element == productId
+      ) >= 0
+    );
   };
 
   const itemRemove = async (productId, callback) => {
-    mutate(
-      {
-        ...wishlist,
-        count:
-          parseInt(get(wishlist, "count", 0)) > 0
-            ? parseInt(get(wishlist, "count")) - 1
-            : 0,
-      },
-      false
-    );
-
-    const wishlistItem = getItem(productId);
-
-    if (!wishlistItem) {
-      await mutate();
-      setIsLoading(false);
-      return;
-    }
-
     setIsLoading(true);
-
     await mutate(
-      await fetch(`/api/lib/v1/wishlist/items/${wishlistItem?.id}`, {
+      await fetch(`/api/lib/v1/wishlist/items/${productId}`, {
         method: "DELETE",
       })
       .then((result) => result.json())
@@ -60,24 +33,15 @@ const useWishlist = () => {
         if (isFunction(callback)) {
           callback(result);
         }
-        setIsLoading(false);
         return result;
       }),
       false
     );
-
     setIsLoading(false);
   };
 
   const itemAdd = async (productId, callback) => {
     setIsLoading(true);
-    mutate(
-      {
-        ...wishlist,
-        count: parseInt(get(wishlist, "count", 0)) + 1,
-      },
-      false
-    );
     try {
       await mutate(
         await fetch(`/api/lib/v1/wishlist/items/${productId}`, {
@@ -88,13 +52,33 @@ const useWishlist = () => {
           if (isFunction(callback)) {
             callback(result);
           }
-          setIsLoading(false);
           return result;
         }),
         false
       );
     } catch (error) {
-      setIsLoading(false);
+      console.error("An unexpected error happened:", error);
+    }
+    setIsLoading(false);
+  };
+
+  const cookieItemsAssign = async (callback) => {
+    setIsLoading(true);
+    try {
+      await mutate(
+        await fetch(`/api/lib/v1/wishlist/items/bulk`, {
+          method: "POST",
+        })
+        .then((result) => result.json())
+        .then((result) => {
+          if (isFunction(callback)) {
+            callback(result);
+          }
+          return result;
+        }),
+        false
+      );
+    } catch (error) {
       console.error("An unexpected error happened:", error);
     }
     setIsLoading(false);
@@ -102,11 +86,12 @@ const useWishlist = () => {
 
   return {
     wishlist,
-    mutateWishlist: mutate,
+    mutateCompare: mutate,
     isLoading: isValidating || isLoading,
     itemAdd,
     itemRemove,
     itemExists,
+    cookieItemsAssign
   };
 };
 
