dea4fff27cdadc49cd3795a1ee74a12dc354908a
Pavol Rasev <pavol@forbestclients.com>
2021-04-08 16:34:26 +0200

    :pencil2: GENERAL - fix wishlist typo in hook

--- hooks/useWishlist.js
+++ hooks/useWishlist.js
@@ -86,7 +86,7 @@ const useWishlist = () => {
 
   return {
     wishlist,
-    mutateCompare: mutate,
+    mutateWishlist: mutate,
     isLoading: isValidating || isLoading,
     itemAdd,
     itemRemove,
