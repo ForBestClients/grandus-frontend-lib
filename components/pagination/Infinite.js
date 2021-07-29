3a17fd343138c1f3375a5157f672c2d936e3e71a
Pavol Rasev <pavol@forbestclients.com>
2021-07-29 21:46:58 +0200

    :sparkles: GENERAL - add items append container for infinite scroll

--- components/pagination/Infinite.js
+++ components/pagination/Infinite.js
@@ -1,6 +1,8 @@
 import { useSWRInfinite } from "swr";
 import map from "lodash/map";
 import LoadingIcon from "components/_other/icons/LoadingIcon";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 // import styles from "./Infinite.module.scss";
 
@@ -12,9 +14,9 @@ const Infinite = ({
   handleGetKey,
   handleGetHref,
   render,
-  className = ''
+  className = "",
+  destinationElementId = null,
 }) => {
-
   const {
     data,
     size: actualPage,
@@ -24,40 +26,55 @@ const Infinite = ({
     initialSize: 0,
   });
 
-  return (
-    <>
-      {map(data, (page, index) => {
-        if (index === 0) {
-          return;
-        }
+  const [isBrowser, setIsBrowser] = useState(false);
 
-        return render(page);
-      })}
-      {actualPage < pageCount ? (
-        <div className={className}>
-          <button
-            className="button"
-            onClick={(e) => {
-              const newPage = actualPage ? actualPage + 1 : 2;
-              e.preventDefault;
-              setActualPage(newPage);
-              handleGetHref(newPage);
-            }}
-          >
-            {isValidating ? (
-              <>
-                <LoadingIcon />
-              </>
-            ) : (
-              "Zobraziť ďalšie produkty"
-            )}
-          </button>
-        </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  if (isBrowser) {
+    return (
+      <>
+        {createPortal(
+          <>
+            {map(data, (page, index) => {
+              if (index === 0) {
+                return;
+              }
+
+              return render(page);
+            })}
+          </>,
+          document.getElementById(destinationElementId)
+        )}
+        {actualPage < pageCount ? (
+          <div className={className}>
+            <button
+              className="button"
+              onClick={(e) => {
+                const newPage = actualPage ? actualPage + 1 : 2;
+                e.preventDefault;
+                setActualPage(newPage);
+                handleGetHref(newPage);
+              }}
+            >
+              {isValidating ? (
+                <>
+                  <LoadingIcon />
+                </>
+              ) : (
+                "Zobraziť ďalšie produkty"
+              )}
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
+
+  return null;
 };
 
 export default Infinite;
