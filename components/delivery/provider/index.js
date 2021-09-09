8b681521f5f68bc3780d0e108999e306e51e1347
matejkopcik <matej.kopcik@gmail.com>
2021-09-09 14:27:05 +0200

    :zap: DELIVERY - added support for custom texts for delivery component

--- components/delivery/provider/index.js
+++ components/delivery/provider/index.js
@@ -1,28 +1,50 @@
 import Packetery, { validationScheme as packeteryValidationScheme } from "./providers/packetery";
+import get from "lodash/get";
 
 export const PACKETERY_TYPE = 1;
 
-export const getValidationScheme = (deliveryProviderType = null) => {
+/*
+getValidationScheme can handle options
+options.text : {
+    Packetery : {
+        validationScheme : "Vyberte odberné miesto"
+    }
+}
+ */
+export const getValidationScheme = (deliveryProviderType = null, options) => {
     if (!deliveryProviderType) {
         return null
     }
 
     switch (deliveryProviderType) {
         case PACKETERY_TYPE:
-            return packeteryValidationScheme;
+            return packeteryValidationScheme(get(options, 'text.Packetery.validationScheme', "Vyberte odberné miesto"));
         default:
             return null;
     }
 }
 
-const DeliveryProvider = ({ deliveryProviderType = null, errors = null, onSelect = null }) => {
+/*
+Delivery provider can handle options
+
+If key in options are from these available:
+ - Packetery
+will be processed to that component
+
+options : {
+    Packetery : {
+        ...
+    }
+}
+ */
+const DeliveryProvider = ({ deliveryProviderType = null, errors = null, onSelect = null , options }) => {
     if (!deliveryProviderType) {
         return null
     }
 
     switch (deliveryProviderType) {
         case PACKETERY_TYPE:
-            return <Packetery errors={errors} onSelect={onSelect} />
+            return <Packetery errors={errors} onSelect={onSelect} options={get(options, 'Packetery', {})}/>
         default:
             return null;
     }
