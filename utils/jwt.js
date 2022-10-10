16229fdf7ea01d23595a6abd97605278a2eaf0ce
Andras Risnyovszky <risnyo96@gmail.com>
2022-10-10 12:05:53 +0200

    :sparkles: JWT - ensure not failing

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,19 +1,33 @@
 import jsonwebtoken from "jsonwebtoken"
+import isEmpty from "lodash/isEmpty";
 
 const jwt = {
   parseJWT: (token) => {
     const secret = process.env.TOKEN_SECRET;
+
+    if (isEmpty(secret)) {
+      return null;
+    }
+
     try {
       return jsonwebtoken.verify(token, secret);
     } catch (e) {
-      console.log(e)
       return null;
     }
   },
 
   signJWT: (data) => {
     const secret = process.env.TOKEN_SECRET;
-    return jsonwebtoken.sign(JSON.stringify(data), secret);
+
+    if (isEmpty(secret)) {
+      return null;
+    }
+
+    try {
+      return jsonwebtoken.sign(JSON.stringify(data), secret);
+    } catch (e) {
+      return null;
+    }
   }
 }
 
