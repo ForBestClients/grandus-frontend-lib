24e75ea89d8180e1bbe43245677e1cd980705378
Ivan Kopčík <ivan@forbestclients.com>
2023-03-10 00:51:23 +0100

    changed conflicted name

--- constants/UrlConstants.js
+++ constants/UrlConstants.js
@@ -2,63 +2,64 @@
 export const RESERVED_URI_PARTS = [
   {
     key: ['priceFrom', 'priceTo'],
-    urlTitle: "cena",
-    title: "Cena", //@todo translation
+    urlTitle: 'cena',
+    title: 'Cena', //@todo translation
     priority: 1,
-    hash: 'price'
+    hash: 'price',
   },
   {
-    key: "storeLocation",
-    urlTitle: "predajna",
-    title: "Predajňa", //@todo translation
+    key: 'storeLocation',
+    urlTitle: 'predajna',
+    title: 'Predajňa', //@todo translation
     priority: 2,
   },
   {
-    key: "availability",
-    urlTitle: "dostupnost",
-    title: "Dostupnosť", //@todo translation
+    key: 'availability',
+    urlTitle: 'dostupnost',
+    title: 'Dostupnosť', //@todo translation
     priority: 3,
   },
   {
-    key: "condition",
-    urlTitle: "trieda",
-    title: "Trieda", //@todo translation
+    key: 'condition',
+    urlTitle: 'trieda',
+    title: 'Trieda', //@todo translation
     priority: 4,
   },
-  { //TODO fix bug with undefined
-    key: "brand",
-    urlTitle: "vyrobca",
-    title: "Výrobca", //@todo translation
+  {
+    //TODO fix bug with undefined
+    key: 'brand',
+    urlTitle: 'vyrobca',
+    title: 'Výrobca', //@todo translation
     priority: 5,
   },
   {
-    key: "status",
-    urlTitle: "stav",
-    title: "Stav", //@todo translation
+    key: 'status',
+    urlTitle: 'status',
+    title: 'Status', //@todo translation
     priority: 6,
   },
   {
-    key: "store",
-    urlTitle: "velkost",
-    title: "Veľkosť", //@todo translation
+    key: 'store',
+    urlTitle: 'velkost',
+    title: 'Veľkosť', //@todo translation
     priority: 7,
   },
   {
-    key: "tags",
-    urlTitle: "tag",
-    title: "Tag", //@todo translations
-    priority: 8
+    key: 'tags',
+    urlTitle: 'tag',
+    title: 'Tag', //@todo translations
+    priority: 8,
   },
   {
-    key: "storeLocationTown",
-    urlTitle: "mesto",
-    title: "Mesto", //@todo translation
+    key: 'storeLocationTown',
+    urlTitle: 'mesto',
+    title: 'Mesto', //@todo translation
     priority: 9,
   },
   {
-    key: "storeLocationDistrict",
-    urlTitle: "okres",
-    title: "Okres", //@todo translation
+    key: 'storeLocationDistrict',
+    urlTitle: 'okres',
+    title: 'Okres', //@todo translation
     priority: 10,
   },
 ];
