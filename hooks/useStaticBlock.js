11c4adf8520a607ab961be4974ce72706cfe47ab
Ivan Kopčík <ivan@forbestclients.com>
2023-06-05 10:50:55 +0200

    added support for specific staticblock api calls

--- hooks/useStaticBlock.js
+++ hooks/useStaticBlock.js
@@ -1,19 +1,55 @@
-import useSWR from "swr";
-import find from "lodash/find";
+import useSWR from 'swr';
+import find from 'lodash/find';
+import isEmpty from 'lodash/isEmpty';
+import join from 'lodash/join';
 
-export default function useStaticBlock() {
+const useStaticBlock = (options = {}) => {
+  let uri = [];
+
+  if (options?.hash) {
+    uri.push('hash=' + options?.hash);
+  }
+
+  if (options?.group) {
+    uri.push('group=' + options?.group);
+  }
+
+  if (options?.fields) {
+    uri.push('fields=' + options?.fields);
+  }
+
+  if (options?.perPage !== false) {
+    uri.push('per-page=' + (options?.perPage ? options?.perPage : '999'));
+  }
+
+  if (options?.expand !== false) {
+    uri.push(
+      'expand=' +
+        (options?.expand ? options?.expand : 'customCss,customJavascript'),
+    );
+  }
+
+  console.log(
+    'old',
+    '/api/lib/v1/blocks?expand=customCss,customJavascript&per-page=999',
+  );
+  console.log(
+    'new',
+    '/api/lib/v1/blocks' + (isEmpty(uri) ? '' : '?' + join(uri, '&')),
+  );
+
   const { data = null, isValidating } = useSWR(
-    "/api/lib/v1/blocks?expand=customCss,customJavascript&per-page=999",
-    (url) => fetch(url).then((r) => r.json()),
+    '/api/lib/v1/blocks' + (isEmpty(uri) ? '' : '?' + join(uri, '&')),
+    url => fetch(url).then(r => r.json()),
     {
       revalidateOnReconnect: false,
       revalidateOnFocus: false,
       shouldRetryOnError: false,
-    }
+    },
   );
 
-  const getByHash = async (hash) => {
-    return find(data, ["hash", hash]) || {};
+  const getByHash = async hash => {
+    return find(data, ['hash', hash]) || {};
   };
 
   const getBy = async (type, hash) => {
@@ -26,4 +62,6 @@ export default function useStaticBlock() {
     getByHash,
     getBy,
   };
-}
+};
+
+export default useStaticBlock;
