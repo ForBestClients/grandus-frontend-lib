ebacebcc6290d17acf06c7bba82c27588ab3c92e
Patrik Spilka <patrik@forbestclients.com>
2023-06-20 09:25:47 +0200

    useCart - added isLoading from SWR and removed loading states

---
+++ hooks/v2/useCart.js
@@ -0,0 +1,330 @@
+import useSWR from 'swr';
+
+import get from 'lodash/get';
+import isFunction from 'lodash/isFunction';
+import filter from 'lodash/filter';
+
+export default function useCart(initialCart = false, options = {}) {
+  const swrOptions = {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  };
+  if (initialCart) {
+    swrOptions.initialData = initialCart;
+    swrOptions.revalidateOnMount = true;
+  }
+
+  const {
+    data: cart,
+    mutate,
+    isValidating,
+    isLoading,
+  } = useSWR(`/api/lib/v1/cart`, url => fetch(url).then(r => r.json()), {
+    ...swrOptions,
+    ...options,
+  });
+
+  const itemRemove = async (itemId, callback) => {
+    mutate(
+      { ...cart, items: filter(cart?.items, item => item?.id !== itemId) },
+      false,
+    );
+    await mutate(
+      await fetch(`/api/lib/v1/cart/items/${itemId}`, {
+        method: 'DELETE',
+      })
+        .then(result => result.json())
+        .then(result => {
+          if (isFunction(callback)) {
+            callback(result);
+          }
+          return result;
+        }),
+      false,
+    );
+  };
+
+  const itemsRemove = async (itemsIds, callback) => {
+    let success = true;
+    const cart = await fetch(`/api/lib/v1/cart/items/bulk`, {
+      method: 'DELETE',
+      body: JSON.stringify({ items: itemsIds }),
+    }).then(async result => {
+      success = result?.ok;
+      const data = await result.json();
+      data.success = success;
+      if (isFunction(callback)) {
+        callback(data);
+      }
+      return data;
+    });
+
+    if (success) {
+      await mutate(cart, false);
+    }
+  };
+
+  const cartUpdate = async (data, callback) => {
+    try {
+      await mutate(
+        await fetch(`/api/lib/v1/cart`, {
+          method: 'POST',
+          body: JSON.stringify({
+            cart: {
+              ...data,
+            },
+          }),
+        })
+          .then(result => result.json())
+          .then(result => {
+            if (isFunction(callback)) {
+              callback(result);
+            }
+            return result;
+          }),
+        false,
+      );
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const cartDestroy = async callback => {
+    try {
+      await mutate(
+        await fetch(`/api/lib/v1/cart`, {
+          method: 'DELETE',
+        }).then(result => {
+          result.json().then(data => {
+            if (isFunction(callback)) {
+              callback(data);
+            }
+
+            return data;
+          });
+        }),
+        false,
+      );
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const itemAdd = async (count, store, productId, callback, options = {}) => {
+    const items = { count: count, sizeId: store, productId: productId };
+    if (options?.hash) {
+      items.hash = get(options, 'hash', '');
+    }
+    try {
+      await itemsAdd(items, callback);
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const itemsAdd = async (items, callback) => {
+    let success = true;
+    const cart = await fetch(`/api/lib/v1/cart`, {
+      method: 'POST',
+      body: JSON.stringify({ items }),
+    }).then(async result => {
+      success = result?.ok;
+      const data = await result.json();
+      data.success = success;
+      if (isFunction(callback)) {
+        callback(data);
+      }
+      return data;
+    });
+
+    if (success) {
+      await mutate(cart, false);
+    }
+  };
+
+  const itemUpdate = async (itemId, body, callback) => {
+    let success = true;
+    const cart = await fetch(`/api/lib/v1/cart/items/${itemId}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        item: body,
+      }),
+    }).then(async result => {
+      success = result?.ok;
+      const data = await result.json();
+      data.success = success;
+      if (isFunction(callback)) {
+        callback(data);
+      }
+      return data;
+    });
+
+    if (success) {
+      await mutate(cart, false);
+    }
+  };
+
+  const saveContact = async (values, callback) => {
+    try {
+      await fetch(`/api/lib/v1/cart/contact`, {
+        method: 'POST',
+        body: JSON.stringify(values),
+      }).then(result => {
+        result.json().then(data => {
+          if (isFunction(callback)) {
+            callback(data);
+          }
+
+          return data;
+        });
+      });
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const removeContact = async callback => {
+    try {
+      await fetch(`/api/lib/v1/cart/contact`, {
+        method: 'DELETE',
+      }).then(result => {
+        result.json().then(data => {
+          if (isFunction(callback)) {
+            callback(data);
+          }
+
+          return data;
+        });
+      });
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const createOrder = async (values, callback) => {
+    try {
+      await fetch(`/api/lib/v1/order/create`, {
+        method: 'POST',
+        body: JSON.stringify(values),
+      }).then(result => {
+        const data = result.json();
+        if (isFunction(callback)) {
+          callback(data);
+        }
+        return data;
+      });
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const applyCoupon = async (couponHash, callback) => {
+    try {
+      const response = await fetch(`/api/lib/v1/cart/coupon`, {
+        method: 'POST',
+        body: JSON.stringify({ couponHash }),
+      }).then(result => {
+        if (isFunction(callback)) {
+          callback(result);
+        }
+        return result.json();
+      });
+      return response;
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    } finally {
+    }
+  };
+
+  const removeCoupon = async callback => {
+    try {
+      const response = await fetch(`/api/lib/v1/cart/coupon`, {
+        method: 'DELETE',
+      }).then(result => {
+        if (isFunction(callback)) {
+          callback(result);
+        }
+        return result.json();
+      });
+
+      return response;
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const applyCredits = async (value, callback) => {
+    try {
+      const response = await fetch(`/api/lib/v1/cart/credits`, {
+        method: 'POST',
+        body: JSON.stringify({ credit: value }),
+      })
+        .then(result => result.json())
+        .then(result => {
+          if (isFunction(callback)) {
+            callback(result);
+          }
+          return result;
+        });
+      setIsLoading(false);
+      return response;
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const applyIsic = async (surname, code, callback) => {
+    try {
+      const response = await fetch(`/api/lib/v1/cart/isic`, {
+        method: 'POST',
+        body: JSON.stringify({
+          isic: {
+            surname,
+            code,
+          },
+        }),
+      })
+        .then(result => result.json())
+        .then(result => {
+          if (isFunction(callback)) {
+            callback(result);
+          }
+          return result;
+        });
+
+      return response;
+    } catch (error) {
+      console.error('An unexpected error happened:', error);
+    }
+  };
+
+  const isProductAdded = productId => {
+    const cartItem = cart?.items?.find(item => item?.product?.id === productId);
+
+    const amount = cartItem ? cartItem?.count || 1 : 0;
+
+    return amount;
+  };
+
+  return {
+    cart: get(cart, 'accessToken') ? cart : null,
+    mutateCart: mutate,
+    isLoading: isLoading,
+    isValidating: isValidating,
+    itemsAdd,
+    itemAdd,
+    itemRemove,
+    itemsRemove,
+    itemUpdate,
+    cartUpdate,
+    cartDestroy,
+    saveContact,
+    removeContact,
+    createOrder,
+    applyCoupon,
+    removeCoupon,
+    applyCredits,
+    applyIsic,
+    isProductAdded,
+  };
+}
