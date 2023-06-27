49d21bf43f1beda1310ab733250cbfa22c5de169
Ivan Kopčík <ivankopcik@Ivans-MacBook-Air.local>
2023-06-27 23:54:34 +0200

    added props methods to cache

--- utils/cache.js
+++ utils/cache.js
@@ -1,32 +1,34 @@
-import get from "lodash/get";
-import split from "lodash/split";
-import zip from "lodash/zip";
-import zipObject from "lodash/zipObject";
-import map from "lodash/map";
-import isNull from "lodash/isNull";
+import get from 'lodash/get';
+import split from 'lodash/split';
+import zip from 'lodash/zip';
+import zipObject from 'lodash/zipObject';
+import map from 'lodash/map';
+import isNull from 'lodash/isNull';
 
-import Redis from "ioredis";
+import Redis from 'ioredis';
+import crypto from 'crypto';
+
 import {
   USER_CONSTANT,
   USER_WISHLIST_CONSTANT,
-} from "grandus-lib/constants/SessionConstants";
+} from 'grandus-lib/constants/SessionConstants';
 
 let client = null;
 if (process.env.CACHE_ENABLED) {
   if (process.env.CACHE_USE_CLUSTER) {
     let clusterConfigExploded = zip(
-      split(process.env.CACHE_PORT, ","),
-      split(process.env.CACHE_HOST, ",")
+      split(process.env.CACHE_PORT, ','),
+      split(process.env.CACHE_HOST, ','),
     );
-    let clusterConfig = map(clusterConfigExploded, (clusterConfigEntry) => {
-      return zipObject(["port", "host"], clusterConfigEntry);
+    let clusterConfig = map(clusterConfigExploded, clusterConfigEntry => {
+      return zipObject(['port', 'host'], clusterConfigEntry);
     });
     client = new Redis.Cluster(clusterConfig);
   } else {
     client = new Redis(
       process.env.CACHE_PORT ? process.env.CACHE_PORT : undefined,
       process.env.CACHE_HOST ? process.env.CACHE_HOST : undefined,
-      { lazyConnect: true }
+      { lazyConnect: true },
     );
   }
 }
@@ -36,12 +38,12 @@ if (process.env.CACHE_ENABLED) {
  *
  * @param {Object} req  - url request object
  */
-const extractUserAccessToken = (req) => {
+const extractUserAccessToken = req => {
   let user = {};
   if (req && req?.session) {
     user = req.session.get(USER_CONSTANT);
   }
-  return get(user, "accessToken", 0);
+  return get(user, 'accessToken', 0);
 };
 
 /**
@@ -56,13 +58,13 @@ export default client;
  */
 export const getCacheKey = (keyParts = []) => {
   return [
-    process.env.CACHE_KEY_PREFIX ? process.env.CACHE_KEY_PREFIX : "prefix",
-    process.env.HOST ? process.env.HOST : "undefined-host",
+    process.env.CACHE_KEY_PREFIX ? process.env.CACHE_KEY_PREFIX : 'prefix',
+    process.env.HOST ? process.env.HOST : 'undefined-host',
     ...keyParts,
-    process.env.CACHE_KEY_SUFFIX ? process.env.CACHE_KEY_SUFFIX : "suffix",
+    process.env.CACHE_KEY_SUFFIX ? process.env.CACHE_KEY_SUFFIX : 'suffix',
   ]
-    .join("-")
-    .replace(/ /gi, "--"); //regex to replace all occurances of ' '
+    .join('-')
+    .replace(/ /gi, '--'); //regex to replace all occurances of ' '
 };
 
 /**
@@ -72,8 +74,15 @@ export const getCacheKey = (keyParts = []) => {
  *
  * @returns {string} User AccessToken or 0
  */
-export const getCacheKeyByRequest = (req) => {
-  return getCacheKey([get(req, "url", "/"), extractUserAccessToken(req)]);
+export const getCacheKeyByRequest = req => {
+  return getCacheKey([get(req, 'url', '/'), extractUserAccessToken(req)]);
+};
+
+export const getCacheKeyByProps = props => {
+  return getCacheKey([
+    'props',
+    crypto.createHash('md5').update(JSON.stringify(props)).digest('hex'),
+  ]);
 };
 
 /**
@@ -82,30 +91,42 @@ export const getCacheKeyByRequest = (req) => {
  * @param {options} type enumerated set of predefined options
  * @param {object} options specified options which variate specific options
  */
-export const getCacheKeyByType = (type = "request", options = {}) => {
+export const getCacheKeyByType = (type = 'request', options = {}) => {
   switch (type) {
-    case "webinstance":
-      return getCacheKey(["system-webinstance"]);
-    case "header":
-      return getCacheKey(["system-layout-header"]);
-    case "footer":
-      return getCacheKey(["system-layout-footer"]);
-    case "custom":
-      const cacheParts = ["custom-key", ...get(options, "cacheKeyParts", [])];
-      if (get(options, "cacheKeyUseUser", true)) {
-        cacheParts.push(extractUserAccessToken(get(options, "req", null)));
+    case 'webinstance':
+      return getCacheKey(['system-webinstance']);
+    case 'header':
+      return getCacheKey(['system-layout-header']);
+    case 'footer':
+      return getCacheKey(['system-layout-footer']);
+    case 'custom':
+      const cacheParts = ['custom-key', ...get(options, 'cacheKeyParts', [])];
+      if (get(options, 'cacheKeyUseUser', true)) {
+        cacheParts.push(extractUserAccessToken(get(options, 'req', null)));
       }
       return getCacheKey(cacheParts);
-    case "request":
-      return getCacheKeyByRequest(get(options, "req", null));
-    case "wishlist":
+    case 'request':
+      return getCacheKeyByRequest(get(options, 'req', null));
+    case 'props':
+      return getCacheKeyByProps(options);
+    case 'wishlist':
       return getCacheKey([
         USER_WISHLIST_CONSTANT,
-        extractUserAccessToken(get(options, "req", null)),
+        extractUserAccessToken(get(options, 'req', null)),
       ]);
     default:
       return getCacheKey([`default-${type}`]);
   }
+};
+
+/**
+ * Get data from Redis cache according to props
+ *
+ * @param {instance} cache sinstance of previosly initiated redis client
+ * @param {object} options specified options which variate specific options
+ */
+export const getCachedDataProps = async (cache, props = {}) => {
+  return await getCachedData({}, cache, { cacheKeyType: 'props', ...props });
 };
 
 /**
@@ -119,10 +140,11 @@ export const getCachedData = async (req, cache, options = {}) => {
   if (!cache) return false;
 
   const cacheKey =
-    getCacheKeyByType(get(options, "cacheKeyType"), { req: req, ...options }) +
+    getCacheKeyByType(get(options, 'cacheKeyType'), { req: req, ...options }) +
     getLocalSuffix(req);
+
   const data = await cache.get(
-    cacheKey
+    cacheKey,
     // (err) => console.error(err)
   );
 
@@ -148,9 +170,25 @@ export const outputCachedData = async (req, res, cache, options = {}) => {
   const cachedData = await getCachedData(req, cache, options);
   if (isNull(cachedData) || cachedData == false) return false;
 
-  res.setHeader("Grandus-Cached-Data", true);
-  res.status(200).json(cachedData);
+  if (!isNull(res)) {
+    res.setHeader('Grandus-Cached-Data', true);
+    res.status(200).json(cachedData);
+  }
   return true;
+};
+
+/**
+ * Save data to Redis cache according to props
+ *
+ * @param {instance} cache sinstance of previosly initiated redis client
+ * @param {object} data data to be saved in cache
+ * @param {object} options specified options which variate specific options
+ */
+export const saveDataToCacheProps = async (cache, data, props = {}) => {
+  return await saveDataToCache({}, cache, data, {
+    cacheKeyType: 'props',
+    ...props,
+  });
 };
 
 /**
@@ -164,23 +202,23 @@ export const outputCachedData = async (req, res, cache, options = {}) => {
 export const saveDataToCache = async (req, cache, data, options = {}) => {
   if (!cache) return false;
 
-  let cacheTime = get(options, "time");
+  let cacheTime = get(options, 'time');
   if (!cacheTime) {
     cacheTime = process.env.CACHE_TIME ? process.env.CACHE_TIME : 60;
   }
 
   const cacheKey =
-    getCacheKeyByType(get(options, "cacheKeyType"), { req: req, ...options }) +
+    getCacheKeyByType(get(options, 'cacheKeyType'), { req: req, ...options }) +
     getLocalSuffix(req);
 
   try {
-    cache.set(cacheKey, JSON.stringify(data), "EX", cacheTime);
+    cache.set(cacheKey, JSON.stringify(data), 'EX', cacheTime);
   } catch (error) {
     console.error(error);
   }
 };
 
-const getLocalSuffix = (req) => {
-  const locale = get(req, "cookies.NEXT_LOCALE");
-  return locale ? `.${locale}` : "";
+const getLocalSuffix = req => {
+  const locale = get(req, 'cookies.NEXT_LOCALE');
+  return locale ? `.${locale}` : '';
 };
