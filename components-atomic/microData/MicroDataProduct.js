48e92c84d6580e466733387d851ca787845d8434
András Risnyovszký <andras@forbestclients.com>
2025-03-28 11:49:16 +0100

    MICRODATA - product backorder availability

--- components-atomic/microData/MicroDataProduct.js
+++ components-atomic/microData/MicroDataProduct.js
@@ -1,6 +1,7 @@
-import { getImageUrl } from "grandus-lib/utils";
-import get from "lodash/get";
-import isEmpty from "lodash/isEmpty";
+import { getImageUrl } from 'grandus-lib/utils';
+import get from 'lodash/get';
+import isEmpty from 'lodash/isEmpty';
+import sum from 'lodash/sum';
 
 const MicroDataProduct = ({ data = null, webInstance = null }) => {
   if (!isEmpty(data) && !isEmpty(webInstance)) {
@@ -13,42 +14,62 @@ const MicroDataProduct = ({ data = null, webInstance = null }) => {
       finalPriceData,
       urlTitle,
       photo,
-      sku = "",
-      ean = "",
+      sku = '',
+      ean = '',
       availability,
     } = data;
     const { domain = null, currency } = webInstance;
+
+    const schemaAvailability = {
+      availability: availability
+        ? 'https://schema.org/InStock'
+        : 'https://schema.org/OutOfStock',
+    };
+
+    if (
+      data?.isOrderable &&
+      process?.env?.NEXT_PUBLIC_BACKORDER_AVAILABILITY_DURATION &&
+      sum(get(data, 'store', []).map(store => store.count)) === 0
+    ) {
+      schemaAvailability.availability = 'https://schema.org/BackOrder';
+      const currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + 14);
+
+      schemaAvailability.availabilityStarts = get(
+        currentDate.toISOString().split('T'),
+        '0',
+      );
+    }
+
     return (
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
-            "@context": "https://schema.org/",
-            "@type": "Product",
+            '@context': 'https://schema.org/',
+            '@type': 'Product',
             productID: `${id}`,
             name,
             image:
-              photo && photo?.path ? getImageUrl(photo, "400x250", "jpg") : "",
-            description: get(shortProductDescription, "description", ""),
+              photo && photo?.path ? getImageUrl(photo, '400x250', 'jpg') : '',
+            description: get(shortProductDescription, 'description', ''),
             url: `${domain}/produkt/${urlTitle}`,
             sku: `${sku}` || `${id}`,
             mpn: `${ean}` || `${id}`,
             brand: {
-              "@type": "Thing",
-              name: get(brand, "name", ""),
+              '@type': 'Thing',
+              name: get(brand, 'name', ''),
             },
             offers: {
-              "@type": "Offer",
+              '@type': 'Offer',
               url: `${domain}/produkt/${urlTitle}`,
               priceCurrency: currency,
-              price: get(finalPriceData, "price", finalPrice),
-              availability: availability
-                ? "https://schema.org/InStock"
-                : "https://schema.org/OutOfStock",
+              price: get(finalPriceData, 'price', finalPrice),
               seller: {
-                "@type": "Organization",
+                '@type': 'Organization',
                 name: domain,
               },
+              ...schemaAvailability,
             },
           }),
         }}
