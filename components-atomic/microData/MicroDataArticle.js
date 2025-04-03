5e939450c930fe2be19ca3a70dc04124b4aa71db
András Risnyovszký <andras@forbestclients.com>
2025-04-03 09:23:36 +0200

    fix article microdata

--- components-atomic/microData/MicroDataArticle.js
+++ components-atomic/microData/MicroDataArticle.js
@@ -1,16 +1,18 @@
-import { getImageUrl } from "grandus-lib/utils";
-import isEmpty from "lodash/isEmpty";
-import upperFirst from "lodash/upperFirst";
+import { getImageUrl } from 'grandus-lib/utils';
+import isEmpty from 'lodash/isEmpty';
+import upperFirst from 'lodash/upperFirst';
+import isArray from 'lodash/isArray';
 
-const convertDateStringToDateObject = (dateString = "") => {
-  const regex = /^(?<date>\d{1,2}).(?<month>\d{1,2}).(?<fullYear>\d{4})\s(?<uTCHours>\d{1,2}):(?<minutes>\d{2}):(?<seconds>\d{2})$/gm
+const convertDateStringToDateObject = (dateString = '') => {
+  const regex =
+    /^(?<date>\d{1,2}).(?<month>\d{1,2}).(?<fullYear>\d{4})\s(?<uTCHours>\d{1,2}):(?<minutes>\d{2}):(?<seconds>\d{2})$/gm;
   const date = new Date();
 
-  const matches = (regex.exec(dateString))?.groups;
+  const matches = regex.exec(dateString)?.groups;
   matches.month -= 1; // numbered from 0 to 11
 
   if (!isEmpty(matches)) {
-    Object.keys(matches).forEach((groupName) => {
+    Object.keys(matches).forEach(groupName => {
       date[`set${upperFirst(groupName)}`](matches[groupName]);
     });
   }
@@ -18,22 +20,22 @@ const convertDateStringToDateObject = (dateString = "") => {
   return date;
 };
 
-const convertDomainToName = (domainString) => {
-  if (!domainString) return ""
+const convertDomainToName = domainString => {
+  if (!domainString) return '';
 
   const hostname = new URL(domainString)?.hostname;
   const domainWithoutWWW = hostname.replace(/^www\./, '');
 
-  return domainWithoutWWW
-}
+  return domainWithoutWWW;
+};
 
 const getLogoUrl = (domain, logoObj) => {
-  if (!domain) return "";
+  if (!domain) return '';
 
   if (logoObj && logoObj?.path) {
     return `${domain}${logoObj.path}`;
   }
-  return "";
+  return '';
 };
 
 const MicroDataArticle = ({ data = null, webInstance = null }) => {
@@ -42,24 +44,26 @@ const MicroDataArticle = ({ data = null, webInstance = null }) => {
   }
 
   const { domain, logo } = webInstance;
-  const { title, photo, createTime } = data;
+  const articles = isArray(data) ? data : [data];
 
-  const schemaArticles = data.map((blog) => ({
-    "@context": "https://schema.org",
-    "@type": "NewsArticle",
+  const schemaArticles = articles.map(blog => ({
+    '@context': 'https://schema.org',
+    '@type': 'NewsArticle',
     headline: blog?.title,
-    image: blog?.photo?.path ? [getImageUrl(blog.photo, "400x250", "jpg")] : [],
+    image: blog?.photo?.path ? [getImageUrl(blog.photo, '400x250', 'jpg')] : [],
     author: {
-      "@type": "Organization",
+      '@type': 'Organization',
       name: convertDomainToName(domain),
-      logo: getLogoUrl(domain,logo),
+      logo: getLogoUrl(domain, logo),
     },
     publisher: {
-      "@type": "Organization",
+      '@type': 'Organization',
       name: convertDomainToName(domain),
-      logo: getLogoUrl(domain,logo),
+      logo: getLogoUrl(domain, logo),
     },
-    datePublished: blog?.createTime ? convertDateStringToDateObject(blog?.createTime) : blog?.publishTime,
+    datePublished: blog?.createTime
+      ? convertDateStringToDateObject(blog?.createTime)
+      : blog?.publishTime,
   }));
 
   return (
