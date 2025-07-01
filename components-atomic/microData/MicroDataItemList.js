4080fc415e1c0c06dd8e2aef8a5c46eedd9d078e
Pavol Rasev <pavol.rasev@gmail.com>
2025-07-01 16:30:57 +0200

    RICHNIPPET - fix category listing

--- components-atomic/microData/MicroDataItemList.js
+++ components-atomic/microData/MicroDataItemList.js
@@ -2,10 +2,14 @@
 import { generateProductScheme } from 'grandus-lib/utils/microdata';
 import map from 'lodash/map';
 import MicroDataList from './MicroDataList';
+import isEmpty from 'lodash/isEmpty';
 
 const MicroDataItemList = ({ data = null, webInstance = null }) => {
+  if (!isEmpty(data) && !isEmpty(webInstance)) {
+    return null;
+  }
   const { products, totalNumberOfItems } = data;
-  const productsScheme = map(products, product => generateProductScheme(product, webInstance));
+  const productsScheme = map(products, product => generateProductScheme(product, webInstance ?? {}));
 
   return <MicroDataList
     listElements={productsScheme}
