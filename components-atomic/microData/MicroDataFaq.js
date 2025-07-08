43e7a45694e961ff91cee69c737fe8b1d669131f
KristianMisiak <kristian@forbestclients.com>
2025-07-08 15:32:14 +0200

    MICRODATA - typo in props

--- components-atomic/microData/MicroDataFaq.js
+++ components-atomic/microData/MicroDataFaq.js
@@ -1,12 +1,12 @@
 import isEmpty from 'lodash/isEmpty';
 
-const MicroDataFAQ = ({ faqs }) => {
-  if (isEmpty(faqs)) return null;
+const MicroDataFAQ = ({ data }) => {
+  if (isEmpty(data)) return null;
 
   const faqStructuredData = {
     '@context': 'https://schema.org',
     '@type': 'FAQPage',
-    mainEntity: faqs.map(faq => ({
+    mainEntity: data.map(faq => ({
       '@type': 'Question',
       name: faq.title,
       acceptedAnswer: {
