de8630ac127a7d24bc2cd95eac18fd4f4e74b856
Ivan Kopčík <ivan.kopcik@gmail.com>
2025-09-22 15:17:24 +0200

    normalized contact fetching

--- hooks/useWebInstance.js
+++ hooks/useWebInstance.js
@@ -1,29 +1,32 @@
-"use client"
-import useSWR from "swr";
-import get from "lodash/get";
-import { ESHOP_TYPE_B2C } from "grandus-lib/constants/AppConstants";
+'use client';
+import useSWR from 'swr';
+import get from 'lodash/get';
+import { ESHOP_TYPE_B2C } from 'grandus-lib/constants/AppConstants';
+import { getContactFromWebInstance } from 'grandus-lib/utils/contact';
 
 export default function useWebInstance() {
   const { data = null } = useSWR(
     `/api/lib/v1/webinstance`,
-    (url) => fetch(url).then((r) => r.json()),
+    url => fetch(url).then(r => r.json()),
     {
       revalidateOnReconnect: false,
       revalidateOnFocus: false,
       shouldRetryOnError: false,
-    }
+    },
   );
+
+  const contact = getContactFromWebInstance(data);
 
   return {
     webInstance: data,
-    domain: get(data, "domain"),
-    settings: get(data, "globalSettings"),
-    eshopType: get(data, "globalSettings.type_of_shop", ESHOP_TYPE_B2C),
+    domain: get(data, 'domain'),
+    settings: get(data, 'globalSettings'),
+    eshopType: get(data, 'globalSettings.type_of_shop', ESHOP_TYPE_B2C),
     contact: {
-      phone: get(data, "globalSettings.phone", ""),
-      tel: get(data, "globalSettings.phone", "").replace(/\s/g, ""),
-      email: get(data, "adminEmail", ""),
-      mailto: get(data, "adminEmail", ""),
+      phone: contact?.phone,
+      tel: contact?.phoneNormalized,
+      email: contact?.email,
+      mailto: contact?.email,
     },
   };
 }
